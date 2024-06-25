a7b9992c05745e39d764e4df1576357448acc6dd
Vidmantas <jaguaras999@gmail.com>
2024-06-25 20:05:43 +0300

    added number conversion

--- Numbers/NumberConversion.cs
+++ Numbers/NumberConversion.cs
@@ -1,16 +1,26 @@
 ﻿using Numbers.Enums;
 using Numbers.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Numbers
 {
     public class NumberConversion
     {
-        private int ConvertHexNumberToInt(string numberToConvert, int numberBase)
+        private static List<int> GetRemaindersOfDecimalNumberDivisions(int number, int numberBase)
+        {
+            var remainders = new List<int>();
+
+            while (number >= numberBase)
+            {
+                remainders.Add(number % numberBase);
+                number /= numberBase;
+            }
+
+            remainders.Add(number);
+
+            return remainders;
+        }
+
+        private static int ConvertHexNumberToInt(string numberToConvert, int numberBase)
         {
             int intNumber = 0;
             var numberToConvertAllUp = numberToConvert.ToUpper();
@@ -30,13 +40,18 @@ namespace Numbers
             return intNumber;
         }
 
-        public string ConvertIntNumberToSelectedNumberBase(int numberToConvert, int numberBase)
+        public static int ConvertIntNumberToSelectedNumberBase(int numberToConvert, int numberBase)
         {
+            int intNumber = 0;
 
+            var remainders = GetRemaindersOfDecimalNumberDivisions(numberToConvert, numberBase);
 
-            return "";
+            for (int i = remainders.Count - 1; i >= 0; i++)
+            {
+                intNumber += remainders[i] * MathCalculationHelper.PowerNumber(numberBase, i);
+            }
+
+            return intNumber;
         }
-
-
     }
 }
